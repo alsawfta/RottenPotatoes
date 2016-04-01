12c72510e3eea6ecd733da0ffe5cd96067fb91d8
alsawfta <nafealsawfta@gmail.com>
2016-04-01 17:42:49 -0500

    added more rspec

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -45,7 +45,7 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.create!(params[:movie])
-    flash[:notice] = "#{@movie.title} was successfully created."
+    flash[:notice] = " was successfully created."
     redirect_to movies_path
   end
 
--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -12,8 +12,8 @@ RSpec.describe MoviesController, :type => :controller do
     end
 
     it 'should go to link with similiar movies' do
-      { :get => "movies/George%20Lucas/moviesWithSameDirector" }.
-          should route_to(:controller => "movies", :action => "moviesWithSameDirector", :find_director => "George Lucas")
+      get :moviesWithSameDirector, :find_director => "George Lucas"
+      route_to(:controller => "movies", :action => "moviesWithSameDirector", :find_director => "George Lucas")
     end
 
     it 'should call the model method that finds similar movies' do
@@ -36,8 +36,9 @@ RSpec.describe MoviesController, :type => :controller do
     end
 
     it 'should go to link with similiar movies' do
-      { :get => "movies/George%20Lucas/moviesWithSameDirector" }.
-          should route_to(:controller => "movies", :action => "moviesWithSameDirector", :find_director => "George Lucas")
+      get :moviesWithSameDirector, :find_director => "George Lucas"
+
+      route_to(:controller => "movies", :action => "moviesWithSameDirector", :find_director => "George Lucas")
     end
 
     it 'should call the model method that finds similar movies' do
@@ -53,38 +54,78 @@ RSpec.describe MoviesController, :type => :controller do
     end
   end
 
-  #
-  # describe "moviesWithSameDirector" do
-  #
-  #
-  #   # it "returns http success" do
-  #   #   get :moviesWithSameDirector
-  #   #   expect(response).to have_http_status(302)
-  #   # end
-  #   #
-  #   it "returns movies with the same director" do
-  #     director_name = @movies.index(0).director_name
-  #     state = false
-  #     @movies.each do |movie|
-  #       if movie.director_name == director_name
-  #         state = true
-  #       end
-  #     end
-  #
-  #     expect(state). to be true
-  #   end
-  #
-  #
-  #
-  #   it "gets all movies with the same director" do
-  #     expect(Movie).to receive(:find_director).with("George Lucas") {@fake_results}
-  #   end
-  #
-  #
-  # end
+  describe 'create ' do
+    it 'render template' do
+      get :new
+      response.should render_template 'new'
+    end
+
+    it 'persist data' do
+      movie =  Movie.any_instance.stub('new movie').as_null_object
+      Movie.should_receive(:create!).and_return(movie)
+
+      post :create, {:movie => movie}
+    end
+
+  end
+
+  describe 'delete ' do
+    it 'render edit template' do
+      Movie.any_instance.stub(:find)
+      get :edit, {:id => 4}
+      response.should render_template 'edit'
+    end
+
+    it 'call update data' do
+      my_movie = double('movie').as_null_object
+
+      Movie.should_receive(:find).and_return(my_movie)
+      my_movie.should_receive(:destroy)
+
+      delete :destroy, {:id => 1}
+    end
 
 
+  end
 
+  describe 'update' do
+    it 'updates the movie' do
+      put :update, id: 1, movie: { title: 'Star Wars' }
+    end
+
+  end
+
+  describe "create" do
+    it "table shows" do
+      post :create, movie: { title: 'Star Wars' }
+    end
+  end
+
+
+  describe "index" do
+    it " title" do
+      get :index, sort: 'title'
+    end
+
+    it "releas date and rating" do
+      get :index, sort: 'release_date', ratings: {'R' => 3 }
+    end
+
+    it "rating is g" do
+      get :index, ratings: {'PG-13' => 2 }
+    end
+
+    it " returns table" do
+      get :index
+    end
+
+  end
+
+  describe "GET show" do
+    it "is ok" do
+      get :show, id: 1
+    end
+  end
 
 
 end
